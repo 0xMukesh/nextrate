86795fe4faf8e942f37e33d6aa0feeb9983d36c1
Kira272921 <sunset272921@gmail.com>
2022-03-27 18:27:56 +0530

    :bug: fix: add intial boilerplate for .jsx and .tsx files

--- bin/utils/readTemplateFiles.js
+++ bin/utils/readTemplateFiles.js
@@ -3,6 +3,16 @@ const fs = require('fs')
 // The `readTemplateFiles` function is used to read the template files from the bin/data/templates folder.
 
 function readTemplateFiles(type, extension) {
+  if (extension === 'jsx') {
+    extension = 'js'
+  }
+  else if (extension === 'tsx') {
+    extension = 'ts'
+  }
+  else {
+    return
+  }
+
   try {
     return fs.readFileSync(`bin/data/templates/${type}-${extension}.txt`, 'utf8')
   }
