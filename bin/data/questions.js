56bc146f6a2c1a7584c938f6856018f8fea6dcb2
Kira272921 <sunset272921@gmail.com>
2022-03-27 17:08:28 +0530

    :sparkles: feat: support for react file extensions

--- bin/data/questions.js
+++ bin/data/questions.js
@@ -1,16 +1,44 @@
-// The initial questions which are been asked to the user when he runs the `nextrate` command
+const fs = require("fs");
 
-const questions = [
-  {
-    type: 'text',
-    name: 'component',
-    message: 'Enter the component name'
-  },
-  {
-    type: 'text',
-    name: 'path',
-    message: 'Enter the folder path where you want to generate the component'
-  }
-]
+const cwd = process.cwd();
+const configFilePath = `${cwd}/.nextrate/config.json`;
+
+var questions;
+
+if (fs.existsSync(configFilePath)) {
+  questions = [
+    {
+      type: 'text',
+      name: 'component',
+      message: 'Enter the component name'
+    },
+    {
+      type: 'text',
+      name: 'path',
+      message: 'Enter the folder path where you want to generate the component'
+    }
+  ]
+}
+
+else {
+  questions = [
+    {
+      type: 'text',
+      name: 'component',
+      message: 'Enter the component name'
+    },
+    {
+      type: 'text',
+      name: 'path',
+      message: 'Enter the folder path where you want to generate the component'
+    },
+    {
+      type: 'input',
+      name: 'config',
+      message: '✨ config: Do you want to use the .jsx and .tsx file format for the component? (y/n)',
+      choices: ['y', 'n'],
+    }
+  ]
+}
 
 module.exports = questions
--- bin/lib/createComponent.js
+++ bin/lib/createComponent.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const listr = require('listr');
 const chalk = require('chalk');
-const shelljs = require('shelljs')
+const shelljs = require('shelljs');
+const objectPath = require('object-path');
 
 const createFile = require('../utils/createFile');
+
+const cwd = process.cwd();
+const configFilePath = `${cwd}/.nextrate/config.json`;
 
 function createComponent(name, type, path) {
   const tasks = new listr([
@@ -14,29 +18,55 @@ function createComponent(name, type, path) {
         // Checking where there is a tsconfig.json file in the current directory
 
         if (!fs.existsSync('tsconfig.json')) {
-          extension = 'js';
-          if (fs.existsSync(path)) {
-            createFile(path, name, type, extension);
+          if (!fs.existsSync(configFilePath)) {
+            extension = 'js';
+            if (fs.existsSync(path)) {
+              createFile(path, name, type, extension);
+            }
+            else {
+              console.log(chalk.red(`🦄 Directory not found! Creating the directory...`));
+              shelljs.mkdir('-p', path);
+              console.log(chalk.green(`🦄 Directory created! Adding the component...`))
+              createFile(path, name, type, extension);
+            }
           }
           else {
-            console.log(chalk.red(`🦄 Directory not found! Creating the directory...`));
-            shelljs.mkdir('-p', path);
-            console.log(chalk.green(`🦄 Directory created! Adding the component...`))
-            createFile(path, name, type, extension);
+            extension = objectPath.get(JSON.parse(fs.readFileSync(configFilePath, "utf8")), "reactExtension");
+            if (fs.existsSync(path)) {
+              createFile(path, name, type, extension);
+            }
+            else {
+              console.log(chalk.red(`🦄 Directory not found! Creating the directory...`));
+              shelljs.mkdir('-p', path);
+              console.log(chalk.green(`🦄 Directory created! Adding the component...`))
+              createFile(path, name, type, extension);
+            }
           }
         }
         else {
-          fileExtension = 'ts';
-          console.log(`Found a tsconfig.json file.`)
-          if (fs.existsSync(path)) {
-            console.log(`🦄 Directory found! Adding the component...`)
-            createFile(path, name, type, extension);
+          if (!fs.existsSync(configFilePath)) {
+            extension = 'ts';
+            if (fs.existsSync(path)) {
+              createFile(path, name, type, extension);
+            }
+            else {
+              console.log(chalk.red(`🦄 Directory not found! Creating the directory...`));
+              shelljs.mkdir('-p', path);
+              console.log(chalk.green(`🦄 Directory created! Adding the component...`))
+              createFile(path, name, type, extension);
+            }
           }
           else {
-            console.log(chalk.red(`🦄 Directory not found! Creating the directory...`))
-            shelljs.mkdir('-p', path);
-            console.log(chalk.green(`🦄 Directory created! Adding the component...`))
-            createFile(path, name, type, extension);
+            extension = objectPath.get(JSON.parse(fs.readFileSync(configFilePath, "utf8")), "reactExtension");
+            if (fs.existsSync(path)) {
+              createFile(path, name, type, extension);
+            }
+            else {
+              console.log(chalk.red(`🦄 Directory not found! Creating the directory...`));
+              shelljs.mkdir('-p', path);
+              console.log(chalk.green(`🦄 Directory created! Adding the component...`))
+              createFile(path, name, type, extension);
+            }
           }
         }
       }
